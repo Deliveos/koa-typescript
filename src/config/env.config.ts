2928e39384a3e0f169fa19ff75ad4b636a6316b6
deliveos <deliveosdars@gmail.com>
2022-02-18 13:08:07 +0600

    Heroku

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,6 +1,6 @@
 const config = {
   PORT: process.env.PORT? Number.parseInt(process.env.PORT) : 5000,
   HOST: process.env.HOST || 'localhost' as string,
-  DB: "mongodb+srv://root:" + process.env.PASS || "root" + "@Q-Delivery.sxyhr.mongodb.net/text?retryWrites=true&w=majority" as string};
+  DB: "mongodb+srv://root:Aw4sB7C94giUxyDx@Q-Delivery.sxyhr.mongodb.net/text?retryWrites=true&w=majority" as string};
 
 export { config };
