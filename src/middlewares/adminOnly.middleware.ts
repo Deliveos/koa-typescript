4253a39c9b66b713c984910aea2ef186328a31ad
deliveos <deliveosdars@gmail.com>
2022-02-18 23:28:42 +0600

    Debug

--- src/middlewares/adminOnly.middleware.ts
+++ src/middlewares/adminOnly.middleware.ts
@@ -8,6 +8,7 @@ const adminOnly = async (ctx: Context, next: Next) => {
   if(ctx.header.authorization !== undefined) {
     const jwtString =  jwt.verify(ctx.header.authorization.split(" ")[1], process.env.SECRET_KEY as Secret) as string;
     logger.log('info', jwtString);
+    console.log(jwtString);
     const decodedToken = JSON.parse(jwtString);
     const database = client.db("Q-Delivery");
     const users = database.collection<User>("users");
