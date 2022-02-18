ae0b4c986e2d69594549f6262299f57f5d74c497
deliveos <deliveosdars@gmail.com>
2022-02-18 12:07:28 +0600

    connect to MongoDB

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,8 +1,6 @@
 const config = {
   PORT: process.env.PORT? Number.parseInt(process.env.PORT) : 5000,
   HOST: process.env.HOST || 'localhost' as string,
-  DB: "mongodb+srv://root:root@test.sxyhr.mongodb.net/node_test?retryWrites=true&w=majority" as string
-  // DB: "mongodb://localhost:27017/node_test" as string
-};
+  DB: "mongodb+srv://root:" + process.env.PASS || "root" + "@Q-Delivery.sxyhr.mongodb.net/text?retryWrites=true&w=majority" as string};
 
 export { config };
