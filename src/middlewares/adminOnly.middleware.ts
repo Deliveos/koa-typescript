747c6e74ccddcf1a4604ca74ab4ce723c4ce3750
deliveos <deliveosdars@gmail.com>
2022-02-19 00:19:24 +0600

    Debug

--- src/middlewares/adminOnly.middleware.ts
+++ src/middlewares/adminOnly.middleware.ts
@@ -3,17 +3,15 @@ import jwt, { JwtPayload, Secret } from 'jsonwebtoken';
 import { client } from '../config/db.config';
 import { User } from '../models/user.model';
 import { logger } from '../utils/logger';
+import { ObjectId } from 'mongodb';
 
 const adminOnly = async (ctx: Context, next: Next) => {
   if(ctx.header.authorization !== undefined) {
     try {
       const token = jwt.verify(ctx.header.authorization, process.env.SECRET_KEY as Secret) as JwtPayload;
-      console.log(token.Id, token.Name);
-      const decodedToken = JSON.parse(jwt.decode(ctx.header.authorization) as string);
-      logger.log('info', decodedToken);
       const database = client.db("Q-Delivery");
       const users = database.collection<User>("users");
-      const user = await users.findOne({ _id: decodedToken.Id, Name: decodedToken.Name })
+      const user = await users.findOne({ _id: new ObjectId(token.Id), Name: token.Name })
       if (user !== null && user.Role.toLowerCase() === "admin") {
         next();
       } else {
