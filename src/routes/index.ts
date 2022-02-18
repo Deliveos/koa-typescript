81b45f0f3705ed22a16f03af80f189ce4c6ee8a4
commanderxa <art.ostrikov@gmail.com>
2022-02-18 15:01:22 +0000

    Events added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import Router from 'koa-router';
 
 import usersRoute from './users.route';
+import eventsRoute from './events.route';
 
 const router = new Router();
 
 router.use(usersRoute.routes());
+router.use(eventsRoute.routes());
 
 export default router;
