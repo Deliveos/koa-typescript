affaf3379ce2af26158d9d67301bfc5bc4e9a8bc
deliveos <deliveosdars@gmail.com>
2022-02-18 13:23:10 +0600

    MongoDB has been connected

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,6 +1,6 @@
 const config = {
   PORT: process.env.PORT? Number.parseInt(process.env.PORT) : 5000,
   HOST: process.env.HOST || 'localhost' as string,
-  DB: "mongodb+srv://root:Aw4sB7C94giUxyDx@Q-Delivery.sxyhr.mongodb.net/text?retryWrites=true&w=majority" as string};
+  DB: "mongodb+srv://root:Aw4sB7C94giUxyDx@q-delivery.sxyhr.mongodb.net/q-delivery?retryWrites=true&w=majority" as string};
 
 export { config };
