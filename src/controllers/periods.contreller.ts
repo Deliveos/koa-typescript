d6f1f6b595b025525e5d64413003ce3f0b823b45
commanderxa <art.ostrikov@gmail.com>
2022-02-19 06:32:48 +0600

    Peroids Data Tests

--- src/controllers/periods.contreller.ts
+++ src/controllers/periods.contreller.ts
@@ -14,10 +14,74 @@ export class PeriodsController {
     var event_typeSuccess = await database.collection<EventType>('event_types').findOne({ "index": 6 });
     var currentDate = new Date();
 
-    var periodDate = ctx.request.query.period;    
+    var periodDate = ctx.request.query.period; 
+    // var ordersByPeriod;
     
+    // switch (periodDate) {
+    //   case "week":
+    //     ordersByPeriod = collection.aggregate([]);
+    //     break;
+
+    //   case "month":
+    //     ordersByPeriod = collection.aggregate([
+    //       {
+    //         $group: {
+    //           "_id": null,
+    //           "21_30_days": {
+    //             $sum: {
+    //               $cond: [
+    //                 { $and: [
+    //                   { $gte: [console.log(new Date("$Date")), new Date().setDate(new Date().getDay()-30)] },
+    //                   { $lte: [new Date("$Date"), new Date().setDate(new Date().getDay()-21)] }
+    //                 ] }, 1, 0
+    //               ]
+    //             }
+    //           },
+    //           "14_21_days": {
+    //             $sum: {
+    //               $cond: [
+    //                 { $and: [
+    //                   { $gte: [new Date("$Date"), new Date().setDate(new Date().getDay()-21)] },
+    //                   { $lte: [new Date("$Date"), new Date().setDate(new Date().getDay()-14)] }
+    //                 ] }, 1, 0
+    //               ]
+    //             }
+    //           },
+    //           "7_14_days": {
+    //             $sum: {
+    //               $cond: [
+    //                 { $and: [
+    //                   { $gte: [new Date("$Date"), new Date().setDate(new Date().getDay()-14)] },
+    //                   { $lte: [new Date("$Date"), new Date().setDate(new Date().getDay()-7)] }
+    //                 ] }, 1, 0
+    //               ]
+    //             }
+    //           },
+    //           "1_7_days": {
+    //             $sum: {
+    //               $cond: [
+    //                 { $gte: [console.log(new Date("$Date")), new Date().setDate(new Date().getDay()-7)] }, 1, 0
+    //               ]
+    //             }
+    //           }
+    //         }
+    //       }
+    //     ]);
+    //     break;
+
+    //   case "year":
+    //     ordersByPeriod = collection.aggregate([]);
+    //     break;
+    // }
+
     const ordersByPeriod = collection.aggregate([
       {
+        $group: {
+          "_id": { "id": "$OrderId.Id" },
+          "Date": { "$max": "$Date" },
+        },
+      },
+      { 
         $group: {
           "_id": null,
           "21_30_days": {
@@ -59,9 +123,9 @@ export class PeriodsController {
           }
         }
       }
-    ]).limit(10);
+    ]);
 
-    var res = await ordersByPeriod.toArray();
+    var res = await ordersByPeriod?.toArray();
     ctx.body = res;
   }
 }
